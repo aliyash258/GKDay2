a97b8681b077825e9ef4c416f472531209c52395
aliyash258 <ashaikh42308@gmail.com>
2025-11-25 13:50:48 -0500

    Created the executor service portion.

--- GKDay2.java
+++ GKDay2.java
@@ -1,7 +1,39 @@
+import java.util.concurrent.*;
+
 public class GKDay2
 {
 	public static void main(String[] args)
 	{
-		Thread[1000] arr = new Thred[1000];
+		try {
+
+			ExecutorService executor = Executors.newFixedThreadPool(1000);
+			int[] nums = new int[1000];
+			for(int i = 0; i < 1000; i++)
+			{
+				Future<Integer> future = executor.submit(new Callable<Integer>() {
+					public Integer call()
+					{
+						int num = 0;
+						for(int i = 0; i < 1000000; i++)
+							num += 1;
+						return num;
+					}
+				});
+				nums[i] = future.get();
+			}
+			int total = 0;
+			for(int i = 0; i < 1000; i++)
+				total += nums[i];
+			System.out.println("Total: " +total);
+			executor.shutdown();
+		}
+		catch(InterruptedException e)
+		{
+			System.err.println(e);
+		}
+		catch(ExecutionException e)
+		{
+			System.err.println(e);
+		}
 	}
 }
